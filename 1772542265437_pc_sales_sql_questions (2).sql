9faa874d154a77940b171d8b2d8eb0cc38c2688f
Your Name <youremail@example.com>
2026-03-18 17:54:22 +0200

    push file

--- 1772542265437_pc_sales_sql_questions (2).sql
+++ 1772542265437_pc_sales_sql_questions (2).sql
@@ -7,7 +7,6 @@
 -- BASIC QUESTIONS
 
 -- 1. Count the total number of sales records.
-SELECT SUM (
 -- 2. Count the number of sales per Continent.
 -- 3. Count the number of sales per Country or State.
 -- 4. List all distinct Shop Name values.
@@ -21,14 +20,56 @@ SELECT SUM (
 -- INTERMEDIATE QUESTIONS
 
 -- 11. Calculate total revenue (SUM of Sale Price).
+SELECT SUM(Sale_price) As total_revenue
+FROM [pc_sales].[dbo].[1772542271737_pc_data (2)]
+
+-- 12. Calculate total profit (SUM of Sale Price - Cost Price).
+SELECT SUM(Sale_price - Cost_price) As total_profit
+FROM [pc_sales].[dbo].[1772542271737_pc_data (2)]
+
 -- 13. Find the average Discount Amount.
+SELECT AVG(Discount_Amount)
+FROM [pc_sales].[dbo].[1772542271737_pc_data (2)]
+
 -- 14. Calculate total Finance Amount issued.
+SELECT SUM(Finance_Amount) As total_Finance_Amount_issued
+FROM [pc_sales].[dbo].[1772542271737_pc_data (2)]
+
+-- 15. Find total revenue per PC Make.
+SELECT PC_Make,SUM(Sale_price) As total_revenue_per_PC_Make
+FROM [pc_sales].[dbo].[1772542271737_pc_data (2)]
+GROUP BY PC_Make
+ORDER BY  total_revenue_per_PC_Make DESC;
 
 -- 16. Find average Sale Price per Storage Type.
+SELECT Storage_type,AVG(Sale_Price)
+FROM [pc_sales].[dbo].[1772542271737_pc_data (2)]
+GROUP BY Storage_type
+ORDER BY storage_type DESC;
+
 -- 17. Calculate total revenue per Shop Name.
+SELECT Shop_Name,SUM(Sale_Price) As total_revenue_per_Shop
+FROM [pc_sales].[dbo].[1772542271737_pc_data (2)]
+GROUP BY Shop_Name
+ORDER BY  total_revenue_per_Shop DESC;
+
 -- 18. Calculate total revenue per Sales Person Name.
--- 19. Find average Credit Score per Payment Method.
+SELECT Sales_Person_Name,SUM(Sale_Price) As total_revenue_per_Sales_Person_Name
+FROM [pc_sales].[dbo].[1772542271737_pc_data (2)]
+GROUP BY Sales_Person_Name
+ORDER BY   total_revenue_per_Sales_Person_Name DESC;
+
+-- 19. Find average Credit Score per Credit Score per Payment Method
+SELECT Payment_Method ,AVG(Credit_Score) As  AVG_Credit_Score
+FROM [pc_sales].[dbo].[1772542271737_pc_data (2)]
+GROUP BY Payment_Method 
+ORDER BY  AVG_Credit_Score DESC
+
 -- 20. Calculate total Cost of Repairs per Sales Person Department.
+SELECT	Sales_Person,Department,SUM (Repairs_Cost) As total_Repair_Cost
+FROM [pc_sales].[dbo].[1772542271737_pc_data (2)]
+GROUP BY Sales_Person,Department;
+
 
 -- ADVANCED QUESTIONS
 
